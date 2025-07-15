295e981ca88c96e2836e747c3ce637f94b580ad6
paradisoilpapa <paradiso.kumamoto@gmail.com>
2025-07-15 15:11:03 +0900

    Update k_app3.py

--- k_app3.py
+++ k_app3.py
@@ -28,7 +28,7 @@ position_multipliers = {
 }
 
 # --- 基本スコア（脚質ごとの基準値） ---
-base_score = {'逃': 1.564, '両': 1.603, '追': 1.834}
+base_score = {'逃': 1.573, '両': 1.683, '追': 1.743}
 
 # --- 状態保持 ---
 if "selected_wind" not in st.session_state:
